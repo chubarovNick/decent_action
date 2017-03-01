b33a5595fe60ac3c77e65d01e0e55ae31b2582ad
Nick Chubarov <nick.chubarov@gmail.com>
2017-03-02 00:11:56 +0400

    Add ruby 2.3.1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'simplecov'
 SimpleCov.start
+
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
 require 'decent_action'
 require 'fixtures/controller_stub'
 require 'fixtures/stub_action'
